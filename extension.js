f1b264f428dbdc2733374e60c4bfaaa56de4a641
Chris <cjmarsh725@gmail.com>
2019-07-27 11:11:16 -0700

    Keybind and class name capture

--- extension.js
+++ extension.js
@@ -3,7 +3,12 @@ const vscode = require('vscode');
 const activate = context => {
   console.log ('className Copier is now active.');
   let disposable = vscode.commands.registerCommand('extension.classNameCopier', () => {
-    vscode.window.showInformationMessage('classNameCopier activated');
+    const editor = vscode.window.activeTextEditor;
+    const document = editor.document;
+    const position = editor.selection.start;
+    const range = document.getWordRangeAtPosition(position, /[a-z\-]+/);
+    const word = document.getText(range);
+    vscode.window.showInformationMessage("classNameCopier: " + word);
   });
   context.subscriptions.push(disposable);
 }
