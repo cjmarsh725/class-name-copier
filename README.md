a84b9a0917147e82bab05cf1a69cd52566b8d943
Chris <cjmarsh725@gmail.com>
2019-07-28 09:23:03 -0700

    Account for case of first line and changed name

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# classNameCopier
+# class-name-copier
 An extension that makes it easier to create CSS classes from JSX classNames. Place the cursor on a class defined in your JSX and activate the command ("extension.classNameCopier", default keybinding is Ctrl+Shift+Alt+C). The class name will be copied to the end of the CSS file of the same name and in the same directory as your component javascript file.
 
--- extension.js
+++ extension.js
@@ -7,22 +7,26 @@ const activate = context => {
     const position = editor.selection.start;
     const range = document.getWordRangeAtPosition(position, /[a-z\-]+/);
     const className = document.getText(range);
-    vscode.window.showInformationMessage("classNameCopier: " + document.fileName.replace(".js", ".css"));
     let testIndex = 0;
+    let isFirstLine = true;
     vscode.window.showTextDocument(vscode.Uri.file(document.fileName.replace(".js", ".css")))
     .then(textDoc => {
       editor = vscode.window.activeTextEditor;
       editor.edit(editorBuilder => {
         const doc = editor.document;
         testIndex = doc.lineCount - 1;
-        while (testIndex >= 0) {
+        while (testIndex > 0) {
           if (doc.lineAt(testIndex).isEmptyOrWhitespace) testIndex -= 1;
           else break;
         }
         const endPos = doc.lineAt(testIndex).range.end;
-        editorBuilder.insert(endPos, "\n\n." + className + " {\n\t\n}");
+        let snippet = "." + className + " {\n\t\n}";
+        isFirstLine = testIndex === 0 && endPos.character === 0;
+        if (!isFirstLine)
+          snippet = "\n\n" + snippet;
+        editorBuilder.insert(endPos, snippet);
       }).then(result => {
-        const newPos = new vscode.Position(testIndex + 3, 1);
+        const newPos = new vscode.Position(testIndex + (isFirstLine ? 1 : 3), 1);
         const newSelection = new vscode.Selection(newPos, newPos);
         editor.selection = newSelection;
         editor.revealRange(newSelection.with());
